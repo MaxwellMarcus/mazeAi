9cf1a8c86d9da0dedc6ebea1499cf4f9712c6ef2
bigmaxattack37 <bigmaxmarcus@gmail.com>
2018-08-31 18:55:59 -0500

    First Commit

---
+++ mazeAI.py
@@ -0,0 +1,184 @@
+#from Tkinter import *
+
+import time
+
+import random
+
+###root = Tk()
+
+#screen_width = #root.winfo_screenwidth()
+#screen_height = #root.winfo_screenheight()
+
+#canvas = Canvas(#root,width = screen_width,height = screen_height)
+#canvas.pack()
+
+creatureList = []
+
+notStarted = True
+
+generation = 0
+
+fitnesses = []
+
+def createNewGen():
+    global creatureList
+    global generation
+
+    createCreatures = 0
+
+    while createCreatures < 1000:
+        _1 = [10,10,110,110,[],0]#,createCreatures]
+
+#        _1 += [canvas.create_rectangle(_1[0],_1[1],_1[2],_1[3],fill = "gray")]
+
+        creatureList.append(_1)
+        createCreatures += 1
+
+    generation += 1
+
+    #finish = canvas.create_rectangle(1210,710,1310,810,fill = "green")
+
+#    border = canvas.create_rectangle(1317,817,4,4,width = 10)
+
+    game = True
+
+def moveRight(player):
+    if len(player[4]) < 20 and not player[2] + 100 > 1310:
+#        canvas.delete(player[6])
+
+
+        player[0] = player[0] + 100
+        player[2] = player[2] + 100
+
+#        player[6] = [canvas.create_rectangle(player[0],player[1],player[2],player[3],fill = "gray")]
+
+def moveLeft(player):
+    if len(player[4]) < 20 and not player[0] - 100 < 0:
+
+#        canvas.delete(player[6])
+
+
+        player[0] = player[0] - 100
+        player[2] = player[2] - 100
+
+#        player[6] = [canvas.create_rectangle(player[0],player[1],player[2],player[3],fill = "gray")]
+
+def moveUp(player):
+    if len(player[4]) < 20 and not player[1] - 100 < 0:
+#        canvas.delete(player[6])
+
+
+        player[1] = player[1] - 100
+        player[3] = player[3] - 100
+
+#        player[6] = [canvas.create_rectangle(player[0],player[1],player[2],player[3],fill = "gray")]
+
+def moveDown(player):
+    if len(player[4]) < 20 and not player[3] + 100 > 810:
+#        canvas.delete(player[6])
+
+
+        player[1] = player[1] + 100
+        player[3] = player[3] + 100
+
+#        player[6] = [canvas.create_rectangle(player[0],player[1],player[2],player[3],fill = "gray")]
+
+def isDone(player):
+    if player[0] == 1210 and player[1] == 710 and player[2] == 1310 and player[3] == 810:
+        player[5] = len(player[4])
+        print("done")
+        print(len(player[4]))
+
+def sort(original):
+    end = []
+
+    x = 0
+    y = 0
+
+    bigest = False
+
+    while x < len(original):
+
+        num = original[x][5]
+
+        if x==0:
+            end += [original[x]]
+            print(str(len(end)) + "," + "begining")
+        elif num < end[0][5]:
+            end.insert(0,original[x])
+            print(str(len(end)) + "," + "begining")
+        elif num >= end[0][5]:
+            bigest = True
+            while y < len(end):
+                if num<end[y][5]:
+                    print(str(len(end)) + "," + "middle")
+                    end.insert(y,original[x])
+                    bigest = False
+                    break
+                y += 1
+            if bigest:
+                print(str(len(end)) + "," + "end")
+                end.append(original[x])
+        else:
+            print("what the hay is happening")
+
+        x+=1
+        y = 0
+    while y < 800:
+        end.remove(end[0])
+        y += 1
+
+    return(end)
+
+def brain(player,generation):
+    global fitnesses
+
+    startTime = time.time()
+
+    done = False
+
+    while len(player[4]) < 20:
+        #time.sleep(1)
+
+        done = False
+        where = random.randint(1,4)
+        if where == 1:
+            moveRight(player)
+            player[4] += [1]
+        elif where == 2:
+            moveLeft(player)
+            player[4] += [2]
+        elif where == 3:
+            moveUp(player)
+            player[4] += [3]
+        elif where == 4:
+            moveDown(player)
+            player[4] += [4]
+        isDone(player)
+        print(str(generation) + " (" + str((player[0] - 10)/100) + "," + str((player[1] - 10)/100) + ")")
+
+    player[5] = ((player[2] - 1310) + (player[3] - 810))/100
+
+
+
+def start(generation):
+    global fitnesses
+    global creatureList
+    moveCreatures = 0
+
+    time.sleep(1)
+    while moveCreatures < len(creatureList):
+        brain(creatureList[moveCreatures],moveCreatures)
+
+        fitnesses += [creatureList[moveCreatures]]
+
+        moveCreatures += 1
+
+    fitnesses = sort(fitnesses)
+
+    print(len(fitnesses))
+    print(fitnesses)
+
+
+createNewGen()
+start(generation)
