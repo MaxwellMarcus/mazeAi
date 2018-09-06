5371c0358c3c42e450257b3ea02b4eb23354b9f0
bigmaxattack37 <bigmaxmarcus@gmail.com>
2018-09-06 18:04:15 -0500

    still rewriting it neater
    almost done with first gen

--- mazeAIv2.py
+++ mazeAIv2.py
@@ -4,7 +4,7 @@ creatureList = []
 numOfGens = 10
 generation = 1
 fitnesses = []
-numCreatures = 10
+numCreatures = 1000
 numMoves = 20
 
 creatureXPos = 0
@@ -20,6 +20,8 @@ down = 4
 playFieldX = 10
 playFieldY  = 10
 
+finnish = [playFieldX,playFieldY]
+
 def move(creature):
     i = 0
     while i < numMoves:
@@ -32,18 +34,20 @@ def move(creature):
             if creature[creatureXPos] > 0:
                 creature[creatureXPos] -= 1
         elif direction == up:
-            if creature[creatureYPos] > 0:
-                creature[creatureXPos] += 1
+            if creature[creatureYPos] < playFieldY:
+                creature[creatureYPos] += 1
         else:
             if creature[creatureYPos] > 0:
-                creature[creatureXPos] += 1
+                creature[creatureYPos] -= 1
+        i += 1
 
 i = 0
-while i < numCreatures
+while i < numCreatures:
     directions = []
     j  = 0
     while j < numMoves:
         directions.append(random.randint(1,4))
+        j += 1
     creature = [0,0,directions,0]
 
     creatureList.append(creature)
@@ -52,5 +56,37 @@ while i < numCreatures
 i = 0
 
 while i < numCreatures:
+    creature = creatureList[i]
+    move(creature)
+    creature[creatureFitness] = (creature[creatureXPos] - playFieldX) + (creature[creatureYPos] - playFieldY)
+    fitnesses.append(creature[creatureFitness])
+
     i += 1
-    move()
+
+end = []
+i = 0
+while i < numCreatures:
+
+    creature = fitnesses[i]
+    fitnessNum = creature[creatureFitness]
+
+    if i == 0:
+        end += [creature]
+
+    elif fitnessNum < end[0][creatureFitness]:
+        end.insert(0,creature)
+
+    elif fitnessNum >= end[0][creatureFitness]:
+        isBigest = True
+        y = 0
+        while y < len(end):
+            if num<end[y][5]:
+                end.insert(y,original[x])
+                bigest = False
+                break
+            y += 1
+        if bigest:
+            end.append(original[x])
+
+    x+=1
+    y = 0
